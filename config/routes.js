f87f757b4011d056a9d43f0d70ee8e022607ee0d
Ma5ee2 <hussainmasee@gmail.com>
2017-10-23 15:43:47 +0100

    navbar works

--- config/routes.js
+++ config/routes.js
@@ -3,50 +3,46 @@ const router  = express.Router();
 const sessionsController = require('../controllers/sessions');
 const registrationController = require('../controllers/registration');
 const addnimesController = require('../controllers/addnimes');
-// const secureRoute = require('../lib/secureRoute');
-//
-// // A home route
+const secureRoute = require('../lib/secureRoute');
+
+// A home route
 router.get('/', (req, res) => res.render('statics/index'));
 
 router.route('/register')
 .get(registrationController.new)
 .post(registrationController.create);
 
-// router.route('/login')
-// .get(sessionsController.new);
-
 router.route('/addnimes')
-  .get(addnimesController.index);
-  // .post(secureRoute, addnimesController.create);
-//
-// router.route('/addnimes/new')
-//   .get(secureRoute, addnimesController.new);
-//
-// router.route('/addnimes/:id')
-//   .get(addnimesController.show)
-//   .put(secureRoute, addnimesController.update)
-//   .delete(secureRoute, addnimesController.delete);
-//
-// router.route('/addnimes/:id/edit')
-//   .get(secureRoute, addnimesController.edit);
-//
-// router.route('/addnimes/:id/comments')
-//   .post(secureRoute, addnimesController.createComment);
-//
-// router.route('/addnimes/:id/comments/:commentId')
-//   .delete(secureRoute, addnimesController.deleteComment);
-//
-// router.route('/register')
-//   .get(registrationController.new)
-//   .post(registrationController.create);
-//
-// router.route('/login')
-//   .get(sessionsController.new)
-//   .post(sessionsController.create);
-//
-// router.route('/logout')
-//   .get(sessionsController.delete);
-//
-// router.all('*', (req, res) => res.notFound());
-//
+  .get(addnimesController.index)
+  .post(secureRoute, addnimesController.create);
+
+router.route('/addnimes/new')
+  .get(secureRoute, addnimesController.new);
+
+router.route('/addnimes/:id')
+  .get(addnimesController.show)
+  .put(secureRoute, addnimesController.update)
+  .delete(secureRoute, addnimesController.delete);
+
+router.route('/addnimes/:id/edit')
+  .get(secureRoute, addnimesController.edit);
+
+router.route('/addnimes/:id/comments')
+  .post(secureRoute, addnimesController.createComment);
+router.route('/addnimes/:id/comments/:commentId')
+  .delete(secureRoute, addnimesController.deleteComment);
+
+router.route('/register')
+  .get(registrationController.new)
+  .post(registrationController.create);
+
+router.route('/login')
+  .get(sessionsController.new)
+  .post(sessionsController.create);
+
+router.route('/logout')
+  .get(sessionsController.delete);
+
+router.all('*', (req, res) => res.notFound());
+
 module.exports = router;
